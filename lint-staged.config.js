16efcba082f02a71f8f2a60e9a125db4d72a39cd
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-03-18 00:50:02 +0800

    testing lintstaged

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-const micromatch = require('micromatch')
-
-module.exports = {
-  '**/*.js?(x)': files => {
-    const match = micromatch.not(files, '*lambda/*.js')
-    return `eslint ${match.join(' ')}`
-  }
-}
