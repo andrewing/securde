3be21cd952b329523db25dfec373dbd0956c9950
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-03-22 02:55:57 +0800

    test

--- functions/user.js
+++ functions/user.js
@@ -1,16 +1,16 @@
-import { CODE } from './util/code';
-import ResponseError from "./util/error"
+import {CODE} from './util/code';
+import ResponseError from './util/error';
 
 export const handler = (event, context, callback) => {
   try {
-    if (event.httpMethod !== 'GET') throw new ResponseError(405, "Method not allowed!")
-    const { q } = event.queryStringParameters
+    if (event.httpMethod !== 'GET')
+      throw new ResponseError(405, 'Method not allowed!');
+    const {q} = event.queryStringParameters;
 
-    const isTaken = q === 'username'
-    callback(null, CODE[200](null, {name:"taken", data: isTaken}))
+    const isTaken = q === 'username';
+    callback(null, CODE[200](null, {name: 'taken', data: isTaken}));
   } catch (err) {
-    const {code, message} = err
-    callback(null,CODE[code||400](message))
+    const {code, message} = err;
+    callback(null, CODE[code || 400](message));
   }
-
 };
