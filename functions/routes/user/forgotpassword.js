540cf9f8f672ddc8760d805116dceca2106df113
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-03-23 21:29:37 +0800

    feat: user functions

---
+++ functions/routes/user/forgotpassword.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import {CODE} from '../../util/code';
+import {SECRET, jwtError} from '../../util/jwt';
+import ResponseError from '../../util/error';
+import {AUDIENCE} from '../../util/constants';
+
+export const forgotpassword = (route, event, context, callback) => {
+  if (event.httpMethod !== 'PUT')
+    throw new ResponseError(405, 'Method not allowed!');
+
+  const {authorization} = event.headers;
+
+  jwt.verify(
+    authorization,
+    SECRET,
+    {audience: AUDIENCE.USER},
+    (err, decoded) => {
+      if (err) throw jwtError(err);
+
+      callback(null, CODE[200](`Successfully changed password`));
+    },
+  );
+};
