7a72a1923adac4dbc0210d79382f2a3b72d0e008
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-03-26 01:10:58 +0800

    feat:register

--- functions/routes/admin/create-manager.js
+++ functions/routes/admin/create-manager.js
@@ -2,25 +2,26 @@ import jwt from 'jsonwebtoken';
 import {CODE} from '../../util/code';
 import {SECRET, jwtError} from '../../util/jwt';
 import ResponseError from '../../util/error';
+import Account from '../../db/models/user';
 import {AUDIENCE} from '../../util/constants';
 
 export const register = (route, event, context, callback) => {
   if (event.httpMethod !== 'POST')
     throw new ResponseError(405, 'Method not allowed!');
-  const body = JSON.parse(event.body);
-  const {authorization} = event.headers;
-
-  jwt.verify(
-    authorization,
-    SECRET,
-    {audience: AUDIENCE.ADMIN},
-    (err, decoded) => {
-      if (err) jwtError(err);
-      if (!body.username) throw new ResponseError(400, 'No Username');
-      if (!body.password) throw new ResponseError(400, 'No Password');
-      // Check if user exists. throw error if it does
-
-      callback(null, CODE(200, 'Successfully created manager!'));
-    },
-  );
+  let body = JSON.parse(event.body);
+  body = {
+    ...body,
+    type: AUDIENCE.BOOK_MANAGER,
+  };
+  Account.findUserByUsername(body.username)
+    .then(found => {
+      if (found) throw new ResponseError(409, 'Username already exists');
+      Account.addAccount(
+        new Account({...body}),
+        callback(null, CODE(200, 'Successfully registered')),
+      );
+    })
+    .catch(err => {
+      throw new ResponseError(500);
+    });
 };
