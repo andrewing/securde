35aeb41b3bea6bdda3ea555cb5cca7021813cbd2
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-04-14 20:03:23 +0800

    feat: testing function added

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,20 @@
+import mongoose from 'mongoose';
 import {CODE} from './util/code';
+import User from './db/models/account';
+
+const dbUrl =
+  'mongodb://uvhzji8iwtsacblx2yjs:wnhhaTah8v5poCUBFy89@bv9wjyqjtkvgm59-mongodb.services.clever-cloud.com:27017/bv9wjyqjtkvgm59';
+const dbOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  // useUnifiedTopology: false,
+};
+// const ERROR = 'error';
+// Set DB from mongoose connection
+mongoose.connect(dbUrl, dbOptions);
 
 export const handler = (event, context, callback) => {
-  callback(null, CODE(200, 'This test is working!'));
+  User.find().then(accounts => {
+    callback(null, CODE(200, 'This test is working!', {accounts}));
+  });
 };
